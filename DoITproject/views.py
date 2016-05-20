3c2b6011de55a531e92afba9af86ae77af292f87
iHelos <ihelos.ermakov@gmail.com>
2016-05-20 15:03:37 +0300

    id GCM

--- DoITproject/views.py
+++ DoITproject/views.py
@@ -57,7 +57,7 @@ class TaskCreate(APIView):
                 Device = get_device_model()
                 device = Device.objects.get(name = task.user_reciever.email)
                 device.send_message(
-                    {'type':'1','title':task.name[:1000], 'text':task.text[:1000], 'user':task.user_creator.email},
+                    {'type':'1','id':task.id, 'title':task.name[:1000], 'text':task.text[:1000], 'user':task.user_creator.email},
                     delay_while_idle=True
                 )
                 return Response({'task': "success"})
